cd0a10530e6439161030f273aede8cf839562107
LuluMaumau <Lucien.MAUVIARD-HAAG@student.isae-supaero.fr>
2020-11-08 01:55:31 +0100

    Start of the DataSearcher implementation with its constructor

--- src/dmanager/DataSearcher.java
+++ src/dmanager/DataSearcher.java
@@ -1,8 +1,56 @@
 package dmanager;
 
+import dtype.*;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.util.HashMap;
+import java.util.Date;
+import java.util.Scanner;
+
 public class DataSearcher {
+
+    HashMap<Date, Data> dateIndex;
+    HashMap<String, Data> satIndex;
+
+    public DataSearcher() throws IOException, ClassNotFoundException {
+        dateIndex = new HashMap<Date, Data>();
+        satIndex = new HashMap<String, Data>();
+
+        /** Completing the satellite list with those already existing in the database */
+        File[] satList = (new File("data")).listFiles();
+        for (File satFile : satList) {
+
+            if (satFile.isDirectory()) {
+
+                File[] dataList = satFile.listFiles();
+                for (File dataFile : dataList) {
+
+                    String dataPath = dataFile.getCanonicalPath();
+                    FileInputStream inDataFile = new FileInputStream(dataPath);
+                    ObjectInputStream inData = new ObjectInputStream(inDataFile);
+                    Data data = (Data) inData.readObject();
+
+                    dateIndex.put(data.getDate(), data);
+                    satIndex.put(data.getSat(), data);
+
+                    inData.close();
+
+                }
+            }
+        }
+    }
 
     public static void main(String[] args) {
 
+        /** Entry command line */
+        Scanner sc = new Scanner(System.in);
+        String s = "";
+        while (!s.equals("quit")) {
+            s = sc.next();
+        }
+        sc.close();
     }
 }
