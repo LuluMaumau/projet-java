1f2c2e3a7239d7b1c5c2b9e763b4e219896e8d24
Estellou06 <74325488+Estellou06@users.noreply.github.com>
2020-11-12 17:17:34 +0100

    Add files via upload

---
+++ src/Extension4/Onglet.java
@@ -0,0 +1,66 @@
+package Extension4;
+
+import java.awt.*;
+import java.util.*;
+
+import javax.swing.*;
+
+import Controle.Database;
+import satellite.*;
+
+public class Onglet extends JPanel {
+
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
+
+    Satellite Satellite;
+    GridBagConstraints gbc;
+    JPanel historique;
+    ArrayList<SousSystemeLabel> SousSystemes;
+
+    public Onglet(Satellite Satellite, JPanel historique, String[] elements) {
+
+        this.Satellite = Satellite;
+        this.historique = historique;
+        SousSystemes = new ArrayList<SousSystemeLabel>();
+
+        new JPanel();
+        GridBagLayout gb = new GridBagLayout();
+        gbc = new GridBagConstraints();
+        setLayout(gb);
+        gbc.fill = GridBagConstraints.NONE;
+        gbc.insets = new Insets(8, 8, 8, 8);
+
+        JComboBox ChoixProcedure = new JComboBox(elements);
+        this.add(ChoixProcedure);
+        JButton Procedure = new JButton(new ProcedureAction(ChoixProcedure, historique, Satellite));
+        this.add(Procedure);
+    }
+
+    public void addLigne(String SousSysteme, int line, JPanel historique, Database BDD) {
+
+        gbc.gridy = line;
+
+        SousSystemeLabel SousSystemeLabel = new SousSystemeLabel(SousSysteme);
+        SousSystemes.add(SousSystemeLabel);
+        this.add(SousSystemeLabel, this.gbc);
+
+        SousSystemeLabel.ChangeCoulour("black");
+
+        JButton ButtonOn = new JButton(new OnAction(this.Satellite, SousSystemeLabel, historique, BDD));
+
+        this.add(ButtonOn, this.gbc);
+
+        JButton ButtonOff = new JButton(new OffAction(this.Satellite, SousSystemeLabel, historique, BDD));
+
+        this.add(ButtonOff, this.gbc);
+
+        JButton ButtonData = new JButton(new DataAction(this.Satellite, SousSystemeLabel, historique, BDD));
+
+        this.add(ButtonData, this.gbc);
+
+    }
+
+}
